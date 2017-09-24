28ef3d5dcae79766a079b11da00fa7e5fa01932b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-09-24 10:09:21 +0900

    ifle

--- mangapdf/main.py
+++ mangapdf/main.py
@@ -1,6 +1,13 @@
 # -*- coding:utf-8 -*-
+import os.path as p
+import os
+
+
+def list_files(pic_dir: str):
+    os.listdir(pic_dir)
+    # os.path.isfile
 
 
 def run(args):
 
-    print(args)
+    print(list_files(args.pic_dir))
--- mangapdf/parser.py
+++ mangapdf/parser.py
@@ -4,6 +4,6 @@ import argparse as ap
 
 def build_parser() -> ap.ArgumentParser:
     parser = ap.ArgumentParser('mangapdf')
-    parser.add_argument('picuture', metavar='PIC_DIR',
+    parser.add_argument('pic_dir', metavar='PIC_DIR',
                         help='directory which includes pictures')
     return parser
