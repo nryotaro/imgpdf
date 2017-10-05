35be6e5d0fda1429daa761c172a43a3dfb8c4f58
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-10-05 09:06:23 +0900

    test build_parser

---
+++ tests/test_parser.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from mangapdf.parser import build_parser
+
+
+def test_build_parser():
+    parser = build_parser()
+
+    d = 'd'
+    parsed = parser.parse_args([d])
+
+    assert parsed.pic_dir == d
